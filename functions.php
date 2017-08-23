0fa2666db221b854f5f33d68a196117bced67b93
Rob Hope <rob@onepagelove.com>
2017-08-23 11:24:25 +0200

    v2.5.8 - Fixed size bug on sidebar name

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /**
  *
  * @package clutterless
- * @since clutterless 2.5.7
+ * @since clutterless 2.5.8
  * @license GPL 2.0
  * 
  */
@@ -11,7 +11,7 @@
 # Theme definitions
 # ------------------------------------------------------------------------
 
-define( 'clutterless_theme_version' , '2.5.7' );  		# Theme version
+define( 'clutterless_theme_version' , '2.5.8' );  		# Theme version
 if ( ! isset( $content_width ) ) $content_width = 600;  # Content Width
 
 # ------------------------------------------------------------------------
